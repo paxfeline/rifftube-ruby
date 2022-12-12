7833a4435c8afaea95a1c039b52dc0f59a95f1c3
paxfeline <paxfeline@gmail.com>
2022-12-12 15:28:03 -0800

    saved riff

--- app/controllers/riffs_controller.rb
+++ app/controllers/riffs_controller.rb
@@ -4,13 +4,14 @@ class RiffsController < ApplicationController
     skip_before_action :verify_authenticity_token
 
     def create
-        print(params[:blob].tempfile)
-        movie = FFMPEG::Movie.new(params[:blob].tempfile)
-        debugger
-        print(movie)
+        movie = FFMPEG::Movie.new(params[:blob].tempfile.path)
 
         movie.transcode("/tmp/movie.mp4") 
+
+        debugger
         
-        @riff = Riff.new(audio_datum: movie)
+        @riff = Riff.new(audio_datum: movie, isText: false, user_id: 1, video_id: 1)
+
+        @riff.save
     end
 end
