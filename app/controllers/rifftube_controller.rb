74b2ae615b707b49ef74783a1f68d8997114d68b
paxfeline <paxfeline@gmail.com>
2022-12-26 20:43:33 -0800

    single file ffmpeg better

--- app/controllers/rifftube_controller.rb
+++ app/controllers/rifftube_controller.rb
@@ -33,25 +33,37 @@ class RifftubeController < ApplicationController
     cursor = 0
     last = 0
     riffs = riffs.sort_by(&:start_time)
-    riffs.each_with_index do |riff, ind|
-      file = Tempfile.new(binmode: true)
-      file.write(riff.audio_datum.to_s)
-      file.close()
-      puts "d=#{riff.start_time - cursor}"
-      if riff.start_time - cursor < 0 then
-        break
+    file_ind = 0
+    riffs.each_slice(30) do |riff_bunch|
+      input_files = ""
+      silences = ""
+      seqs = ""
+      n = riff_bunch.count * 2 # silence + riff
+      riff_bunch.each_with_index do |riff, ind|
+        file = Tempfile.new(binmode: true)
+        file.write(riff.audio_datum.to_s)
+        file.close()
+        input_files << " -i #{file.path}"
+        silences << "aevalsrc=exprs=0:d=#{riff.start_time - cursor}[silence#{ind}], "
+        seqs << "[silence#{ind}] [#{ind}:a] "
+        cursor = riff.start_time + riff.duration
       end
-      if cursor == 0 then
+      #puts input_files
+      puts silences
+      puts seqs
+      puts "====="
+      if file_ind == 0 then
         # this should always happen first
-        `ffmpeg -y -i #{file.path} -filter_complex "aevalsrc=exprs=0:d=#{riff.start_time}[silence], [silence] [0:a] concat=n=2:v=0:a=1[outa]" -map [outa] /tmp/mixing#{ind.to_s}.mp4`
+        #`ffmpeg -y -i #{file.path} -filter_complex "aevalsrc=exprs=0:d=#{riff.start_time}[silence], [silence] [0:a] concat=n=2:v=0:a=1[outa]" -map [outa] /tmp/mixing#{ind.to_s}.mp4`
+        `ffmpeg -y #{input_files} -filter_complex "#{silences}#{seqs}concat=n=#{n}:v=0:a=1[outa]" -map [outa] /tmp/mixing#{file_ind.to_s}.mp4`
       else
-        `ffmpeg -y -i /tmp/mixing#{ind - 1}.mp4 -i #{file.path} -filter_complex "aevalsrc=exprs=0:d=#{riff.start_time - cursor}[silence], [0:a] [silence] [1:a] concat=n=3:v=0:a=1[outa]" -map [outa] /tmp/mixing#{ind}.mp4`
+        #`ffmpeg -y -i /tmp/mixing#{ind - 1}.mp4 -i #{file.path} -filter_complex "aevalsrc=exprs=0:d=#{riff.start_time - cursor}[silence], [0:a] [silence] [1:a] concat=n=3:v=0:a=1[outa]" -map [outa] /tmp/mixing#{ind}.mp4`
+        `ffmpeg -y -i /tmp/mixing#{file_ind - 1}.mp4 #{input_files} -filter_complex "#{silences}#{seqs}concat=n=#{n + 1}:v=0:a=1[outa]" -map [outa] /tmp/mixing#{file_ind.to_s}.mp4`
       end
-      last = ind
-      cursor = riff.start_time + riff.duration
-      puts cursor.to_s
+      last = file_ind
+      file_ind = file_ind + 1
     end
-    single_data = File.read('/tmp/mixing#{last}.mp4');
+    single_data = File.read("/tmp/mixing#{last}.mp4");
     send_data single_data
   end
 end
